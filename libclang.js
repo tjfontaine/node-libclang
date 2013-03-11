217c0104f6449825fa10b6ea559178d93c719f51
Timothy J Fontaine <tjfontaine@gmail.com>
2013-03-11 10:27:23 -0700

    allow module to actually be require'd

---
+++ libclang.js
@@ -0,0 +1,6 @@
+exports.Cursor = require('./lib/cursor');
+exports.Index = require('./lib/index');
+exports.TranslationUnit = require('./lib/translationunit');
+exports.Type = require('./lib/type');
+exports.Location = require('./lib/location');
+exports.generate = require('./lib/generateffi').generate;
