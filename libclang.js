c98764c6f4cfc11314049ca0bf5709c956a0f483
Matt Budish <matt.budish@digikey.com>
2017-05-26 08:18:20 -0500

    Revert "Convert to ES6 syntax."
    This reverts commit 54c24157845aa243a0ee7a0ac87eaf323feaf829.

--- libclang.js
+++ libclang.js
@@ -18,33 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE
 
-var lib = require('./dynamic_clang');
-var util = require('./util');
-var ref = require('ref');
-
-var Location = function (instance) {
-  if (!(this instanceof Location))
-    return new Location(instance)
-
-  //TODO XXX FIXME clang_getNullLocation
-  this._instance = instance;
-
-  Object.defineProperty(this, 'presumedLocation', {
-    get: function () {
-      var self = this;
-      var file = ref.alloc(lib.CXString);
-      var line = ref.alloc(ref.types.uint32);
-      var column = ref.alloc(ref.types.uint32);
-      lib.libclang.clang_getPresumedLocation(self._instance,
-        file, line, column);
-
-      return {
-        filename: util.toString(file.deref()),
-        line: line.deref(),
-        column: column.deref(),
-      };
-    },
-  });
-};
-
-module.exports = Location;
+exports.Cursor = require('./lib/cursor');
+exports.Index = require('./lib/index');
+exports.TranslationUnit = require('./lib/translationunit');
+exports.Type = require('./lib/type');
+exports.Location = require('./lib/location');
